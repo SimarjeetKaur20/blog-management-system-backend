6ea5fdabc85ec0082f88573fc31f12fa106a3c4a
Simarjeet Kaur <simarjeetkaur20012006@gmail.com>
2026-03-03 10:40:51 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
-
 // REGISTER
 exports.register = async (req, res) => {
   try {
@@ -11,7 +10,6 @@ exports.register = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
-
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
