f8f0f0223d8918279efc5a2cae6ee3ce8613ff96
Simarjeet Kaur <simarjeetkaur20012006@gmail.com>
2026-03-03 10:41:13 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,5 @@
 // middleware/authMiddleware.js
 const jwt = require("jsonwebtoken");
-
 module.exports = (req, res, next) => {
   const token = req.headers["authorization"];
   if (!token) return res.status(401).send("Access denied");
