9a01d4036c7d6488a6dcf04a31a000cc3ea70282
Simarjeet Kaur <simarjeetkaur20012006@gmail.com>
2026-03-12 21:13:57 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,7 +6,6 @@ module.exports = (req, res, next) => {
     return res.status(401).json({ message: "No token provided" });
   }
   const token = authHeader.split(" ")[1];
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // 🔥 THIS LINE IS CRITICAL
